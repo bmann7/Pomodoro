6bac3acd5c64991321386245a1bd05adb6d94ee8
Brandon Mannarino <brandon.mannarino@gmail.com>
2021-09-16 22:11:08 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import "./App.css";
+//import "./App.css";
 import Header from "./components/Header.jsx";
 import TimerComponent from "./components/Circle-timer.jsx";
+import ContactForm from "./components/Contact-form.jsx";
 
 function App() {
   return (
     <div>
-      <Header className="center" />
+      <Header />
 
       {/* Circular-timer component */}
-      <TimerComponent className="center" />
+      <TimerComponent />
+      <br />
+      <hr />
+      <br />
+      <ContactForm />
+      <br />
     </div>
   );
 }
